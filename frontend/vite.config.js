bfd16f83ca0952699d51fc0a9912e3f5580dc4e6
Unni Krishnan B V <unnik9218@gmail.com>
2026-01-03 20:00:13 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // important: tells Vite to build into 'dist'
-  },
-  base: './', // ensures paths work correctly in production
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'), // optional: for cleaner imports
-    },
-  },
-})
+  base: './', // Important for static deploy
+});
